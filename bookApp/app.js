24484fda3adee7153298e8bc7722dfa1913e109d
alldsouz <allen-bernard.dsouza@capgemini.com>
2022-06-27 16:41:10 +0530

    4:30PM practice

---
+++ bookApp/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/mongoosejs';
+
+const app = express();
+
+mongoose.connect(url, {useNewUrlParser:true});
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected...');
+});
+
+app.use(express.json());
+
+// const alienRouter = require('./routers/aliens')
+// app.use('/aliens', alienRouter);
+
+app.listen(1000,function(){
+    console.log('server started at port 1000');
+});
