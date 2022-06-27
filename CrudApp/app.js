95c43a95614a56b6616bed4210107b3cff744bd0
alldsouz <allen-bernard.dsouza@capgemini.com>
2022-06-27 10:57:43 +0530

    11AM

--- CrudApp/app.js
+++ CrudApp/app.js
@@ -7,6 +7,15 @@ const app = express();
 mongoose.connect(url, {useNewUrlParser:true});
 const con = mongoose.connection
 
-con.on('open', function(){
-    console.log('connected...')
+con.on('open', () => {
+    console.log('connected...');
+});
+
+app.use(express.json());
+
+const alienRouter = require('./routers/aliens')
+app.use('/aliens', alienRouter);
+
+app.listen(9000,function(){
+    console.log('server started at port 9000');
 });
---
+++ CrudApp/routers/aliens.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Alien = require('../models/alien');
+
+//get request
+router.get('/', async(req, res) => {
+    try{
+        const aliens = await Alien.find()
+        res.json(aliens);
+    }catch(err){
+        res.send('Error ' + err)
+    }
+});
+
+//post request
+router.post('/',async(req,res)=>{
+    const alien = new Alien({
+        name:req.body.name,
+        tech:req.body.tech
+    });
+    try{
+        const a1 = await alien.save();
+        res.json(a1);
+    }catch(err){
+        res.send("error");
+    }
+});
+
+module.exports = router
