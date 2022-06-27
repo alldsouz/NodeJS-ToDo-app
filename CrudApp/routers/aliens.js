a9b3eb80b3217c5d0ea6e17653c92879913e1b26
alldsouz <allen-bernard.dsouza@capgemini.com>
2022-06-27 12:02:10 +0530

    12PM

--- CrudApp/routers/aliens.js
+++ CrudApp/routers/aliens.js
@@ -12,6 +12,16 @@ router.get('/', async(req, res) => {
     }
 });
 
+//find by id
+router.get('/:id', async(req, res) => {
+    try{
+        const alien = await Alien.findById(req.params.id)
+        res.json(alien);
+    }catch(err){
+        res.send('Error ' + err)
+    }
+});
+
 //post request
 router.post('/',async(req,res)=>{
     const alien = new Alien({
@@ -23,6 +33,40 @@ router.post('/',async(req,res)=>{
         res.json(a1);
     }catch(err){
         res.send("error");
+    }
+});
+
+//patch/update data by id
+router.patch('/:id', async(req,res)=>{
+    try{
+        const alien= await Alien.findById(req.params.id);
+        alien.tech = req.body.tech
+        const a1 = await alien.save();
+        res.json(a1)
+    }catch(err){
+        res.send('Error')
+    }
+});
+
+// //delete data by id
+// router.patch('/:id', async(req,res)=>{
+//     try{
+//         const alien= await Alien.findById(req.params.id);
+//         alien.tech = req.body.tech
+//         const a1 = await alien.remove();
+//         res.json(a1)
+//     }catch(err){
+//         res.send('Error')
+//     }
+// });
+
+//delete try 2
+router.delete('/:id', async(req,res)=>{
+    try{
+        const alienDelete = await Alien.deleteOne({_id:req.params.id})
+        res.send('alien with id ' + req.params.id +' removed')
+    }catch(e){
+        res.send(e)
     }
 });
 
